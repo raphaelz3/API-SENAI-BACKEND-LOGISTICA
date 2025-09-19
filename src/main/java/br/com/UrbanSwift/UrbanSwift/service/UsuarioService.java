53fda5b2a2e94d031a2b9cbbda7d587f8416e035
leandrocsilva36 <leandro.c.silva36@aluno.senai.br>
2025-09-18 21:54:31 -0300

    Criação CRUD

--- src/main/java/br/com/UrbanSwift/UrbanSwift/service/UsuarioService.java
+++ src/main/java/br/com/UrbanSwift/UrbanSwift/service/UsuarioService.java
@@ -1,8 +1,53 @@
 package br.com.UrbanSwift.UrbanSwift.service;
 
+
+import br.com.UrbanSwift.UrbanSwift.model.Usuario;
+import br.com.UrbanSwift.UrbanSwift.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
 
+    public UsuarioService(UsuarioRepository user) {
+        usuarioRepository = user;
+    }
+
+    public List<Usuario> listarTodosUsuarios() {
+        return usuarioRepository.findAll();
+    }
+    //Buscar por ID
+    public Usuario buscarUsuarioPorId(Integer idUsuario) {
+        return usuarioRepository.findById(idUsuario).orElse(null);
+    }
+    //Criar
+    public Usuario cadastrarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+    //Deletar
+    public Usuario deletarUsuarioPorId(Integer idUsuario) {
+        Usuario c =  buscarUsuarioPorId(idUsuario);
+        if(c == null){
+            return null;
+        }
+        usuarioRepository.deleteById(idUsuario);
+        return c;
+    }
+    //Editar
+    public Usuario atualizarUsuarioPorId(Integer idUsuario,  Usuario usuario) {
+        Usuario c =  buscarUsuarioPorId(idUsuario);
+        if(c == null){
+            return null;
+        }
+
+        c.setTipoUsuario(usuario.getTipoUsuario());
+        c.setEmail(usuario.getEmail());
+        c.setSenha(usuario.getSenha());
+        c.setNomeCompleto(usuario.getNomeCompleto());
+
+        usuarioRepository.save(c);
+        return  c;
+    }
 }
--- src/main/java/br/com/UrbanSwift/UrbanSwift/service/VeiculosService.java
+++ src/main/java/br/com/UrbanSwift/UrbanSwift/service/VeiculosService.java
@@ -1,8 +1,55 @@
 package br.com.UrbanSwift.UrbanSwift.service;
 
+
+
+import br.com.UrbanSwift.UrbanSwift.model.Veiculo;
+import br.com.UrbanSwift.UrbanSwift.repository.VeiculosRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VeiculosService {
+    private final VeiculosRepository veiculosRepository;
+
+    public VeiculosService(VeiculosRepository user) {
+        veiculosRepository = user;
+    }
+
+    public List<Veiculo> listarTodosVeiculos() {
+        return veiculosRepository.findAll();
+    }
+    //Buscar por ID
+    public Veiculo buscarVeiculosPorId(Integer idVeiculos) {
+        return veiculosRepository.findById(idVeiculos).orElse(null);
+    }
+    //Criar
+    public Veiculo cadastrarVeiculos(Veiculo veiculos) {
+        return veiculosRepository.save(veiculos);
+    }
+    //Deletar
+    public Veiculo deletarVeiculoPorId(Integer idVeiculo) {
+        Veiculo c =  buscarVeiculosPorId(idVeiculo);
+        if(c == null){
+            return null;
+        }
+        veiculosRepository.deleteById(idVeiculo);
+        return c;
+    }
+    //Editar
+    public Veiculo atualizarVeiculosPorId(Integer idVeiculo,  Veiculo veiculo) {
+        Veiculo c =  buscarVeiculoPorId(idVeiculo);
+        if(c == null){
+            return null;
+        }
+
+        c.setModelo(veiculo.getModelo());
+        c.setPlaca(veiculo.getPlaca());
+        c.setTipo(veiculo.getTipo());
+        c.setTipoUsuarios(veiculo.getTipoUsuarios());
+
+        veiculosRepository.save(c);
+        return  c;
+    }
 
 }

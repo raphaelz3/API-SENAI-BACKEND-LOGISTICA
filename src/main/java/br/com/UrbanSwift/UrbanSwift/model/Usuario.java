c94d073e5bb30242bba907741d22028e193892ac
Raphaelz3 <raphael.g.nascimento@aluno.senai.br>
2025-09-19 19:59:46 -0300

    Implementação de segurança

--- src/main/java/br/com/UrbanSwift/UrbanSwift/model/Usuario.java
+++ src/main/java/br/com/UrbanSwift/UrbanSwift/model/Usuario.java
@@ -1,10 +1,18 @@
 package br.com.UrbanSwift.UrbanSwift.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,7 +21,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "usuario")
 
-public class Usuario {
+public class Usuario implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_usuario", nullable = false)
@@ -30,4 +38,45 @@ public class Usuario {
     @JoinColumn(name = "id_tipo_usuario")
     private TipoUsuario tipoUsuario;//a FK é do tipo TipoUsuario, pois é chave estrangeira dessa tabela
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(tipoUsuario.getDescricao()));
+    }
+
+    @Override
+    @JsonIgnore
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
 }
